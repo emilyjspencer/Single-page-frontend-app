613658da0c2b4372151f853eb85d87233b266cbf
Emily <emilyspencer89@hotmail.co.uk>
2020-03-31 18:59:56 +0100

    Adds text. Text and Dr Jeyll picture disappear when the picture of Jekyll is clicked. An image of Mr Hyde and more text appears

--- jekyllhyde.js
+++ jekyllhyde.js
@@ -11,6 +11,7 @@
     
     function scary() {
       document.getElementById("drjekyll").style.visibility = "hidden";
+      document.getElementById("jekylldrinks").style.visibility = "hidden";
         document.body.appendChild(document.createElement("img")).src = "Public/mrhyde.jpg";
-    
+        document.body.appendChild(document.createElement("div")).innerText = "“O God!” I screamed, and “O God!” again and again; for there before my eyes—pale and shaken, and half fainting, and groping before him with his hands, like a man restored from death—there stood Henry Jekyll!"
     };
