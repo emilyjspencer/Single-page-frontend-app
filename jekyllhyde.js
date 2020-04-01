7cca931604d3fbb0747707e22e83a4ea7dcd9587
Emily <emilyspencer89@hotmail.co.uk>
2020-04-01 09:17:19 +0100

    Adds more styling

--- jekyllhyde.js
+++ jekyllhyde.js
@@ -17,9 +17,12 @@
       document.getElementById("jekylldrinks").style.visibility = "hidden";
       document.getElementById("firstclick").style.visibility = "hidden"; 
         document.body.appendChild(document.createElement("img")).src = "Public/mrhyde.jpg"; 
-        var h = document.createElement("p")              
+        var h = document.createElement("h3")              
         var t = document.createTextNode(`“O God!” I screamed, and “O God!” again and again; for there before my eyes—pale and shaken, and half fainting, and groping before him with his hands, like a man restored from death—there stood Henry Jekyll!"`); 
         h.appendChild(t);
         document.body.appendChild(h);
     
       };
+
+       
+   
